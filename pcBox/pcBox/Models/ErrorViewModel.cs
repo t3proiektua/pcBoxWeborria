9a118fa4ae62ed13c6ef3ec9097933ed0b252fc6
Iker Jauregi <jauregi.iker@uni.eus>
2023-02-06 08:13:35 +0100

    Web basea

---
+++ pcBox/pcBox/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace pcBox.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
